5824b80936696cb9dc7b7c5ff7d9029db32ea57c
Jallal-Malik <140415632+Jallal-Malik@users.noreply.github.com>
2023-10-31 16:25:44 +0000

    datatypes in Js

---
+++ Hitesh-JS-1/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all Js code as a newer version
+// alert( 3 + 3) // we are using nodejs not a browser
+
+console.log( 3 
+    + 
+      3) // code readability are not good
+
+console.log( 3 + 4)
+
+let name = "Hitesh"
+let age = 18
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => no value
+// symbol => unique
+
+// object
+
+console.log(typeof "Hitesh"); // => string
+console.log(typeof age); // => number
+console.log(typeof null); // => object
+console.log(typeof undefined); // => undefined
