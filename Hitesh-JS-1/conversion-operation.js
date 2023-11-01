9a166c2042087c3d90221ceae4443dd5533d70be
Jallal-Malik <140415632+Jallal-Malik@users.noreply.github.com>
2023-11-01 17:20:45 +0000

    conversion of datatypes

--- Hitesh-JS-1/conversion-operation.js
+++ Hitesh-JS-1/conversion-operation.js
@@ -7,35 +7,35 @@ let zero = true
 let one = false
 let two = "Hitesh"
 
-console.log(typeof number);
-console.log(typeof (number));
+// console.log(typeof number);
+// console.log(typeof (number));
 
 let value = Number(score)
-console.log(typeof value);
-console.log(value);
+// console.log(typeof value);
+// console.log(value);
 
 let Hy = Number(empty)
-console.log(typeof Hy);
-console.log(Hy);
+// console.log(typeof Hy);
+// console.log(Hy);
 
 let By = Number(zero)
-console.log(typeof By);
-console.log(By);
+// console.log(typeof By);
+// console.log(By);
 
 let Bye = Number(one)
-console.log(typeof Bye);
-console.log(Bye);
+// console.log(typeof Bye);
+// console.log(Bye);
 
 let Hye = Number(two)
-console.log(typeof Hye);
-console.log(Hye);
+// console.log(typeof Hye);
+// console.log(Hye);
 
 let valueNumber = Number(string)
-console.log(valueNumber);
+// console.log(valueNumber);
 
 let valueInNumber = Number(notANumber)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -46,16 +46,52 @@ let isLogIn = 0 // => false
 let isSignIn = "Hitesh" // => true
 
 let BooleanisLoggedIn = Boolean(isLoggedIn)
-console.log(BooleanisLoggedIn);
+// console.log(BooleanisLoggedIn);
 
 let BooleanIn = Boolean(isLogIn)
-console.log(BooleanIn);
+// console.log(BooleanIn);
 
 let BooleanIs = Boolean(isSignIn)
-console.log(BooleanIs);
+// console.log(BooleanIs);
 
 let someNumber = 33
 
 let numberSome = String(someNumber)
-console.log(numberSome);
-console.log(typeof (numberSome));
+// console.log(numberSome);
+// console.log(typeof (numberSome));
+
+// *********************** Operations ***********************
+
+let posValue = 3
+let negValue = -posValue
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 1 + 2);
+// console.log(1 + 1 + "2");
+
+// console.log((3 + 4) * 5 % 10);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3 
+num1 = num2  = num3 = 2 + 2
+
+let consoler = 100
+++consoler;
+console.log(consoler);
