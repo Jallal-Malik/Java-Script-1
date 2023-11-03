5a407ba24b79320187753fa22e3a7c6d007ac8ea
Jallal-Malik <140415632+Jallal-Malik@users.noreply.github.com>
2023-11-03 09:04:02 +0000

    datatypes-summary

--- Hitesh-JS-1/datatypes-summary.js
+++ Hitesh-JS-1/datatypes-summary.js
@@ -8,5 +8,29 @@ const isLoggedIn = false
 const temp = null
 let email;
 
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 341565213654654546n
+
 //  Refrence (Non primitive)
 
+const arrow = ["sun", "date", "time"];
+
+let myobj = {
+    name: "jallal",
+    age: 18,
+}
+
+let myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof temp);
+console.log(typeof scorevalue);
+console.log(typeof myFunction);
+console.log(typeof myobj);
+console.log(typeof id);
