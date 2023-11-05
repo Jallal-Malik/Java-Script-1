cc697ac6cbcebb982e267f3b492568e15b4cabff
Jallal-Malik <140415632+Jallal-Malik@users.noreply.github.com>
2023-11-05 06:10:38 +0000

    strings in js

--- Hitesh-JS-1/05_strings.js
+++ Hitesh-JS-1/05_strings.js
@@ -4,3 +4,29 @@ const numbers = 50
 // console.log(name + numbers + " hy");
 
 console.log(`My name is ${name} and my numbers is ${numbers}`);
+
+const gameName = new String("Hitesh")
+
+console.log(gameName [0] );
+console.log(gameName.__proto__ );
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("t"));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne =("    Hitesh    ")
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://jallalmalik.com/strings%20discussion"
+console.log(url.replace('%20', '-'))
+console.log(url.includes("hy"))
+
+const gameNameOne = new String("Hitesh.hc.com")
+console.log(gameNameOne.split('.'));
